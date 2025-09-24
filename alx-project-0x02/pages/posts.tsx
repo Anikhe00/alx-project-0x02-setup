f54db01e75c8afab9122ce5631da236db0a37922
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-24 16:02:30 +0100

    Add static posts

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,28 +1,13 @@
 import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 import { type PostProps } from "@/interfaces";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const Posts: React.FC = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+interface PostsPageProps {
+  posts: PostProps[];
+}
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response: Response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      const data = await response.json();
-
-      const formattedPosts: PostProps[] = data.map((post) => ({
-        title: post.title,
-        content: post.body,
-        userId: post.userId,
-      }));
-      setPosts(formattedPosts);
-    };
-    fetchPosts();
-  }, []);
-
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
   return (
     <div className="flex flex-col gap-2">
       <Header />
@@ -44,3 +29,21 @@ const Posts: React.FC = () => {
 };
 
 export default Posts;
+
+// ✅ Use getStaticProps to fetch posts at build time
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  const formattedPosts: PostProps[] = data.map((post: any) => ({
+    title: post.title,
+    content: post.body,
+    userId: post.userId,
+  }));
+
+  return {
+    props: {
+      posts: formattedPosts,
+    },
+  };
+}
