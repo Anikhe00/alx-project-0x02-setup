4c4fc3b0656c565ab29775731ab93c16b1377059
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-24 11:08:01 +0100

    Create a Reusable Card Component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,10 @@
+import Card from "@/components/common/Card";
+
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Home Section</h1>
+      <Card title="Home Card" content="This is the home card content." />
     </div>
   );
 };
