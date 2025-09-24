a769dc4ffa5fb1e5d7eb66d5a8380d69bfdc21ad
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-24 15:59:35 +0100

    Implement a PostCard Component

---
+++ alx-project-0x02/components/common/PostCard.tsx
@@ -0,0 +1,13 @@
+import { type PostProps } from "@/interfaces";
+
+const PostCard: React.FC<PostProps> = ({ title, content, userId }) => {
+  return (
+    <div className="bg-white border p-4 rounded-md shadow-md">
+      <h2 className="text-lg font-bold mb-2">{title}</h2>
+      <p className="text-gray-700">{content}</p>
+      <span className="text-sm text-gray-500">User ID: {userId}</span>
+    </div>
+  );
+};
+
+export default PostCard;
--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,11 +1,43 @@
 import Header from "@/components/layout/Header";
+import PostCard from "@/components/common/PostCard";
+import { type PostProps } from "@/interfaces";
+import React, { useEffect, useState } from "react";
 
 const Posts: React.FC = () => {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response: Response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+
+      const formattedPosts: PostProps[] = data.map((post) => ({
+        title: post.title,
+        content: post.body,
+        userId: post.userId,
+      }));
+      setPosts(formattedPosts);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="flex flex-col gap-2">
       <Header />
-      <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
+      <div className="min-h-screen p-6 bg-gray-50">
+        <h1 className="text-3xl font-bold mb-6">Posts</h1>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {posts.map((post, index) => (
+            <PostCard
+              key={index}
+              title={post.title}
+              content={post.content}
+              userId={post.userId}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
